7c9010cd307f7a0c987e1e18a66f1ca8ef8ca800
이다현 <pop8261@naver.com>
2022-01-25 17:15:07 +0900

    문자열 내 마음대로 정렬하기

--- Programmers/src/src/pg12915.java
+++ Programmers/src/src/pg12915.java
@@ -1,4 +1,20 @@
-
+import java.util.Arrays;
+import java.util.Comparator;
 public class pg12915 {
-
+	public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            public int compare(String str1, String str2){
+                if(str1.charAt(n)==str2.charAt(n)){
+                    return str1.compareTo(str2);
+                }
+                else if(str1.charAt(n)>str2.charAt(n)){
+                    return 1;
+                }
+                else{
+                    return -1;
+                }
+            }
+        });
+        return strings;
+    }
 }

e59d7beafdb01dbb8546d671e04338c3ce8f0811
이다현 <pop8261@naver.com>
2022-01-25 17:26:57 +0900

    두 개 뽑아서 더하기

---
+++ Programmers/src/src/Programmers/pg68644.java
@@ -0,0 +1,25 @@
+package Programmers;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class pg68644 {  
+	public int[] solution(int[] numbers) {
+        ArrayList<Integer> sum = new ArrayList<>();
+        int result;
+        for(int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length;j++){
+                if(i!=j){
+                    result = numbers[i] + numbers[j];
+                    if(!sum.contains(result)){
+                        sum.add(result);
+                    }
+                }
+            }
+        }
+        int[] answer = new int[sum.size()];
+        for(int k=0;k<sum.size();k++){
+            answer[k] = sum.get(k);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}

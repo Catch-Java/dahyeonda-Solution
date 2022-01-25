86bf5d909e7434604fdba833d3f911aff0c79916
이다현 <pop8261@naver.com>
2022-01-25 17:25:35 +0900

    평균 구하기

---
+++ Programmers/src/src/Programmers/pg12944.java
@@ -0,0 +1,12 @@
+package Programmers;
+public class pg12944 {
+	public double solution(int[] arr) {
+        double answer = 0;
+        int total=0;
+        for(int i=0;i<arr.length;i++){
+            total+=arr[i];
+        }
+        answer=(double)total/arr.length;
+        return answer;
+    }
+}
